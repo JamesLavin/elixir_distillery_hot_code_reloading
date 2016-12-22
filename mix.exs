fc4b2510d18da4e299e6bfb68443048a6e240aac
James Lavin <jlavin@teladoc.com>
2016-12-22 15:24:39 -0500

    improve app; bump version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixEnvSettings.Mixfile do
 
   def project do
     [app: :phoenix_env_settings,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
